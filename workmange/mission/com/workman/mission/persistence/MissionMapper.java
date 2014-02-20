02a58af9c28cc807d91690f99ea667fa11d5725f
unknown <windy@.(none)>
2014-02-20 22:19:31 +0800

    modify mission modual

--- workmange/mission/com/workman/mission/persistence/MissionMapper.java
+++ workmange/mission/com/workman/mission/persistence/MissionMapper.java
@@ -7,10 +7,10 @@ import com.workman.mission.persistence.wrapper.MissionWrapper;
 
 public interface MissionMapper {
 	List<MissionModel> getMissions(MissionWrapper wrapper);
+	List<MissionModel> getPendingMissions(int handlerId);
 	MissionModel getMission(int missionId);
 	void create(MissionModel mission);
 	void update(MissionModel mission);
 	void commitMission(int missionId);
-	
 	int getCount(MissionWrapper wrapper);
 }
